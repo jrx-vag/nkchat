a5e5d0552854572566f690163d025561d8c2b6e1
Carlos Gonzalez <carlosj.gf@gmail.com>
2017-09-14 19:34:02 +0200

    Delete & obj_name change
    Signed-off-by: Carlos Gonzalez <carlosj.gf@gmail.com>

--- src/objs/nkchat_message_obj_type_view.erl
+++ src/objs/nkchat_message_obj_type_view.erl
@@ -23,7 +23,7 @@
 -module(nkchat_message_obj_type_view).
 -author('Carlos Gonzalez <carlosj.gf@gmail.com>').
 
--export([view/1, subview/2, table_data/2]).
+-export([view/1, subview/2, table_data/2, element_updated/3]).
 
 -include("nkchat.hrl").
 -include_lib("nkadmin/include/nkadmin.hrl").
@@ -252,14 +252,14 @@ table_iter([Entry|Rest], Pos, Acc, Session) ->
     table_iter(Rest, Pos+1, [Data|Acc], Session).
 
 
-%%%% @private
-%%element_updated(_ObjId, Value, _Session) ->
-%%    #{
-%%        <<"text">> := Text
-%%    } = Value,
-%%    Update = #{
-%%        ?CHAT_MESSAGE => #{
-%%            text => Text
-%%        }
-%%    },
-%%    {ok, Update}.
+%% @private
+element_updated(_ObjId, Value, _Session) ->
+    #{
+        <<"text">> := Text
+    } = Value,
+    Update = #{
+        ?CHAT_MESSAGE => #{
+            text => Text
+        }
+    },
+    {ok, Update}.
