290ba74dd8edb6a301bc285ea48e4284e8008e5d
Valentín Adolfo Acosta García <valentin.acosta@jaraxa.net>
2019-01-22 11:07:46 +0100

    Added a new "chat.announcement" message type

--- include/nkchat.hrl
+++ include/nkchat.hrl
@@ -34,7 +34,7 @@
 -define(CHAT_MSG_TYPE_ADDED_MEMBER, <<"chat.member_added">>).
 -define(CHAT_MSG_TYPE_REMOVED_MEMBER, <<"chat.member_removed">>).
 -define(CHAT_MSG_TYPE_ALERT, <<"chat.alert">>).
-
+-define(CHAT_MSG_TYPE_ANNOUNCEMENT, <<"chat.announcement">>).
 
 -define(DEFAULT_SILENT, false).
 -define(DEFAULT_READ_PREVIOUS, false).
